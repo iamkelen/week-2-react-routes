91f0b82f878e860897da0bd0114b3d2a4c2ebcc4
iamkelen <gkalashnick@gmail.com>
2020-09-24 18:30:37 +0300

    Fixed some bugs

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -9,7 +9,7 @@ const Dashboard = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <div id="title">Dashboard</div>
-          <div><Link to="/dashboard/profile/ce1971e0-6b21-4728-8efe-5369a4c49aad">Go To Dashboard</Link></div>
+          <div><Link to="/dashboard/profile/ce1971e0-6b21-4728-8efe-5369a4c49aad">Go To Profile</Link></div>
           <div><Link to="/dashboard/main">Go To Main</Link></div>
         </div>
       </div>
